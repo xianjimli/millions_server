c4c882a3721727eaf19ed55b35c0ea0261c2dee8
lixianjing <xianjimli@hotmail.com>
2017-10-15 12:29:41 +0800

    add mqttd

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # million_server
 test server support millions concurrent connections.
+
+```
+git submodule init
+git submodule update
+cd 3rd/nginx/src/
+./auto/configure
+cd -
+```
--- src/mqtt/mqtt_service.cpp
+++ src/mqtt/mqtt_service.cpp
@@ -1,4 +1,6 @@
 #include <errno.h>
+#include "MQTTPacket.h"
+#include "MQTTConnect.h"
 
 #include "utils.h"
 #include "mqtt_service.h"
@@ -22,32 +24,171 @@ bool mqtt_service_deinit() {
     return true;
 }
 
+#define SMALL_BUFF_LEN 256
+#define LARGE_BUFF_LEN 4096
+
+extern "C" int MQTTSerialize_zero(unsigned char* buf, int buflen, unsigned char packettype);
+
+bool mqtt_on_connect(worker_t* w, unsigned char* buf, size_t len) {
+	int ret = 0;
+	MQTTPacket_connectData c;	
+	unsigned char out[SMALL_BUFF_LEN];
+
+    return_value_if_fail(MQTTDeserialize_connect(&c, buf, len), false);
+
+    printf("%s\n", __func__);
+
+	ret = MQTTSerialize_connack(out, sizeof(out), 0, 0);
+	if(ret > 0) {
+		ret = write(w->fd, out, ret);
+	} 
+
+	return ret >= 0;
+}
+
+bool mqtt_on_publish(worker_t* w, unsigned char* buf, size_t len) {
+	int ret = 0;
+    int qos = 0;
+    int payloadlen = 0;
+    MQTTString topicName;
+    unsigned char dup = 0;
+    unsigned char retained = 0;
+    unsigned short packetid = 0;
+    unsigned char* payload = NULL;
+	unsigned char out[SMALL_BUFF_LEN];
+
+    ret = MQTTDeserialize_publish(&dup, &qos, &retained, &packetid, &topicName, &payload, &payloadlen, buf, len);
+    return_value_if_fail(ret, false);
+
+    /*TODO*/
+    printf("%s\n", __func__);
+
+    ret = MQTTSerialize_puback(out, sizeof(out), packetid);
+
+	if(ret > 0) {
+		ret = write(w->fd, out, ret);
+	} 
+
+	return ret >= 0;
+}
+
+#define MAX_TOPIC_NR 16
+const int grantedQoSs[MAX_TOPIC_NR] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+bool mqtt_on_subcribe(worker_t* w, unsigned char* buf, size_t len) {
+	int ret = 0;
+    int maxcount = 1;
+    int count = MAX_TOPIC_NR;
+    unsigned char dup = 0;
+    unsigned short packetid = 0;
+	unsigned char out[SMALL_BUFF_LEN];
+    int requestedQoSs[MAX_TOPIC_NR];
+    MQTTString topicFilters[MAX_TOPIC_NR];
+
+    ret = MQTTDeserialize_subscribe(&dup, &packetid, maxcount, &count, topicFilters, requestedQoSs, buf, len);
+    return_value_if_fail(ret, false);
+
+    /*TODO*/
+
+    ret = MQTTSerialize_suback(out, sizeof(out), packetid, count, (int*)grantedQoSs); 
+	if(ret > 0) {
+		ret = write(w->fd, out, ret);
+	} 
+
+	return ret >= 0;
+}
+
+bool mqtt_on_unsubcribe(worker_t* w, unsigned char* buf, size_t len) {
+	int ret = 0;
+	int count = 0;
+	unsigned char dup = 0;
+	unsigned short packetid = 0;
+	int max_count = MAX_TOPIC_NR;
+	unsigned char out[SMALL_BUFF_LEN];
+	MQTTString topicFilters[MAX_TOPIC_NR];
+
+	ret = MQTTDeserialize_unsubscribe(&dup, &packetid, max_count, &count, topicFilters, buf, len);
+    return_value_if_fail(ret, false);
+    
+	/*TODO*/
+
+    ret = MQTTSerialize_unsuback(out, sizeof(out), packetid);
+	if(ret > 0) {
+		ret = write(w->fd, out, ret);
+	} 
+	
+	return ret >= 0;
+}
+
+bool mqtt_on_ping(worker_t* w, unsigned char* buf, size_t len) {
+	int ret = 0;
+	unsigned char out[SMALL_BUFF_LEN];
+    
+    ret = MQTTSerialize_zero(out, sizeof(out), PINGRESP);
+	if(ret > 0) {
+		ret = write(w->fd, out, ret);
+	} 
+	
+	return ret >= 0;
+}
+
+bool mqtt_on_disconnect(worker_t* w, unsigned char* buf, size_t len) {
+	int ret = 0;
+	unsigned char out[SMALL_BUFF_LEN];
+    
+    ret = MQTTSerialize_zero(out, sizeof(out), DISCONNECT);
+	if(ret > 0) {
+		ret = write(w->fd, out, ret);
+	} 
+	
+	return ret >= 0;
+}
+
+bool mqtt_on_stats(worker_t* w, unsigned char* buf, size_t len) {
+	return true;
+}
+
 bool mqtt_worker_work(worker_t* w) {
-    char buf[1024];
+    unsigned char buf[4096];
     int fd = w->fd;
-    int wret = 0;
     int rret = read(fd, buf, sizeof(buf));
 
     if (rret > 0) {
+    	unsigned char msg_type = buf[0] >> 4;
+
         s_stat.requests++;
         s_stat.recv_bytes += rret;
 
-        if(strncmp(buf, "stats", 5) == 0) {
-            size_t speed = s_stat.requests/(time(0)-s_stat.start);
-            snprintf(buf, sizeof(buf), "requests=%zu speed=%zur/s send=%zu recv=%zu\n", 
-                s_stat.requests, speed, s_stat.send_bytes, s_stat.recv_bytes);
-            rret = strlen(buf);
-        }
-        
-        wret = write(fd, buf, rret);
-        s_stat.send_bytes += wret;
+		switch(msg_type) {
+			case PUBLISH: {
+				return mqtt_on_publish(w, buf, rret);
+			}
+			case CONNECT: {
+				return mqtt_on_connect(w, buf, rret);
+			}
+			case SUBSCRIBE: {
+				return mqtt_on_subcribe(w, buf, rret);
+			}
+			case UNSUBSCRIBE: {
+				return mqtt_on_unsubcribe(w, buf, rret);
+			}
+			case PINGREQ: {
+				return mqtt_on_ping(w, buf, rret);
+			}
+			case DISCONNECT: {
+				return mqtt_on_disconnect(w, buf, rret);
+			}
+			case 's': {
+				return mqtt_on_stats(w, buf, rret);
+			}
+			default: {
+				printf("unkown packet, close client.\n");
+				return false;
+			}
+		}
     }
 
-    if (rret <= 0 || wret <= 0){
-        return false;
-    }
-
-    return true;
+	return rret > 0;
 }
 
 bool mqtt_worker_init(worker_t* w) {
